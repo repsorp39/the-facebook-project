e8b0f690cb3c7ba3fd04d1948fbdd1e08255bab7
Prosper <hazardprosper227@gmail.com>
2025-07-06 12:46:37 +0100

    Conflict resolve for adins/mod

--- backend/routes/admin/moderator.get.php
+++ backend/routes/admin/moderator.get.php
@@ -13,17 +13,13 @@ use App\PostService\Post;
 use App\UserService\User;
 
 if ($_SERVER["REQUEST_METHOD"] === "GET") {
-    try {
-        $userid = decodeTokenFromHeader();
-        $Auth = new Auth($userid);
+    $userid = decodeTokenFromHeader();
+    $Auth = new Auth($userid);
 
-        if (!$userid) return JSON::serve(401, ["message" => "Connexion requise"]);
-        if (!$Auth->isAdmin()) return JSON::serve(403, ["message" => "Acces interdit"]);
+    if (!$userid) return JSON::serve(401, ["message" => "Connexion requise"]);
+    if (!$Auth->isAdmin()) return JSON::serve(403, ["message" => "Acces interdit"]);
 
-        $User = new User();
-        $Moderators = $User->getAllModerators();
-        JSON::serve(200, $Moderators);
-    } catch (Exception $e) {
-        JSON::serve(500, ["error" => $e->getMessage()]);
-    }
+    $User = new User();
+    $Moderators = $User->getAllModerators();
+    JSON::serve(200, $Moderators);
 }
--- backend/routes/moderator/users.get.php
+++ backend/routes/moderator/users.get.php
@@ -11,17 +11,13 @@ use App\AuthService\Auth;
 use App\UserService\User;
 
 if ($_SERVER["REQUEST_METHOD"] === "GET") {
-    try {
-        $userid = decodeTokenFromHeader();
-        $Auth = new Auth($userid);
+    $userid = decodeTokenFromHeader();
+    $Auth = new Auth($userid);
 
-        if (!$userid) return JSON::serve(401, ["message" => "Connexion requise"]);
-        if (!$Auth->isModerator()) return JSON::serve(403, ["message" => "Acces interdit"]);
+    if (!$userid) return JSON::serve(401, ["message" => "Connexion requise"]);
+    if (!$Auth->isModerator()) return JSON::serve(403, ["message" => "Acces interdit"]);
 
-        $User = new User();
-        $users = $User->getAllAvailableUsers();
-        JSON::serve(200,$users);
-    } catch (Exception $e) {
-        JSON::serve(500, ["error" => $e->getMessage()]);
-    }
+    $User = new User();
+    $users = $User->getAllAvailableUsers();
+    JSON::serve(200,$users);
 }
