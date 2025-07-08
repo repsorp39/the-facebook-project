01a6b64766c06439a52b65b08dd4f17f065ca14f
Prosper <hazardprosper227@gmail.com>
2025-07-08 21:11:15 +0100

    Ajout de la navbar mobile

--- frontend/src/components/MobileNavBar.jsx
+++ frontend/src/components/MobileNavBar.jsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/images/brandmark.png";
-import { NavLink, Link, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-import { Tooltip } from "react-tooltip";
 import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../store/features/user-slice";
+import { FaGear } from "react-icons/fa6";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
 
 import {
   House,
   MessageCircleMore,
   UsersRound,
   BellRing,
-  Settings,
-  EllipsisVertical,
-  LogOutIcon,
+  User,
 } from "lucide-react";
 
 const NavBar = () => {
@@ -21,6 +21,8 @@ const NavBar = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const [isHidden, setHidden] = useState(true);
+  const toggleVisibility = () => setHidden(!isHidden);
   const items = [
     {
       path: "/",
@@ -42,74 +44,71 @@ const NavBar = () => {
       label: "Notifications",
       icon: <BellRing />,
     },
+    {
+      path: `/profile/${user.userid}`,
+      label: "Profil",
+      icon: <User />,
+    },
+    {
+      path: `/settings`,
+      label: "Paramètres",
+      icon: <FaGear />,
+    },
   ];
-  const pathname = useLocation().pathname;
+
   return (
-    <header className='fixed hidden top-0 w-full h-16 bg-white shadow-sm z-20 md:flex items-center justify-between px-6 border-b border-gray-200'>
+    <header className='flex fixed w-full flex-col bg-white h-16 shadow-sm z-20 md:hidden px-3 border-b border-gray-200'>
       {/* Logo */}
-      <section className='flex items-center gap-6'>
-        <img src={logo} alt='Logo' className='w-40 h-auto object-contain cursor-pointer' />
-      </section>
-
-      {/* Icônes de navigation */}
-      <section className='w-1/3'>
-        <ul className='flex items-center justify-between gap-4'>
-          {items.map((item, index) => (
-            <li key={item.label} className='relative'>
-              <NavLink
-                to={item.path}
-                id={item.label}
-                className={`rounded-md hover:bg-gray-100 transition [&_svg]:w-5 [&_svg]:h-5 w-full ${
-                 (pathname === item.path || (pathname.startsWith(item.path) && item.path.length > 1))
-                    ? "bg-blue-100 text-blue-600"
-                    : "text-gray-700"
-                }`}
-              >
-                {item.icon}
-              </NavLink>
-              <Tooltip anchorSelect={`#${item.label}`} content={item.label} />
-            </li>
-          ))}
-        </ul>
-      </section>
-
-      {/* Profil utilisateur */}
-      <section className='flex items-center gap-3 cursor-pointer'>
-        <img
-          src={user.picture}
-          alt='Profil'
-          onClick={() => navigate(`/profile/${user.userid}`)}
-          className='w-10 h-10 rounded-full object-cover border'
-        />
-        <div className='relative'>
-          <button
-            id='more'
-            className='p-2 rounded-full hover:bg-gray-100 transition'
-          >
-            <EllipsisVertical className='text-gray-600' />
-          </button>
-          <Tooltip
-            anchorSelect='#more'
-            clickable={true}
-            className='bg-white border rounded-md shadow-md overflow-hidden p-0'
-          >
-            <div className='w-56'>
-              <Link
-                to='/settings'
-                className='flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition'
-              >
-                <Settings className='w-4 h-4' /> Paramètres
-              </Link>
-              <button
-                onClick={() => dispatch(logoutUser())}
-                className='flex items-center gap-2 px-4 py-2 w-full text-sm text-white bg-red-600 hover:bg-red-500 transition'
-              >
-                <LogOutIcon className='w-4 h-4' /> Se déconnecter
-              </button>
-            </div>
-          </Tooltip>
+      <section className='flex items-center justify-between h-16'>
+        <div>
+          <img
+            src={logo}
+            alt='Logo'
+            className='w-40 h-auto object-contain cursor-pointer'
+          />
+        </div>
+        <div className='inline-block cursor-pointer transition' onClick={toggleVisibility}>
+          {isHidden ? (
+            <GiHamburgerMenu className='h-8 w-8' />
+          ) : (
+            <IoClose className='h-8 w-8' />
+          )}
         </div>
       </section>
+
+      {!isHidden && (
+        <nav className='mb-5 z-30 bg-gray-900 p-3 text-sm shadow-md text-white rounded-md mt-2'>
+          <ul>
+            {items.map((item) => {
+              const classname =
+                "text-white font-medium me-3 flex justify-start items-center w-full p-3";
+
+              return (
+                <li className='w-full flex flex-col' key={item.label}>
+                  <NavLink
+                    className={(link) =>
+                      link.isActive
+                        ? `${classname} bg-blue-500 w-full rounded-sm`
+                        : `${classname}`
+                    }
+                    to={item.path}
+                  >
+                    {" "}
+                    <span className='me-3'>{item.icon}</span>{" "}
+                    <span className='block'>{item.label}</span>{" "}
+                  </NavLink>{" "}
+                </li>
+              );
+            })}
+            <li className='w-full flex flex-col mt-3'>
+              <button onClick={()=>dispatch(logoutUser())} className='bg-red-800 text-white rounded-xl font-semibold p-3'>
+                {" "}
+                Se déconnecter{" "}
+              </button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
