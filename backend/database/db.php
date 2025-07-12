9acf198a6547225b7a22e5e1c4644871bed2dec9
Prosper <hazardprosper227@gmail.com>
2025-07-12 09:41:26 +0100

    Adapté le fuseau horaire de la bdd à celui du Benin

--- backend/database/db.php
+++ backend/database/db.php
@@ -5,15 +5,17 @@ function database():PDO{
     if($bdd) return $bdd;
     
     try{
+        // .env normalement mais pour des modifications futures sera pris en compte
         $host = 'shinkansen.proxy.rlwy.net';      // Host Railway
         $port = '59178';                          // Port Railway
         $dbname = 'railway';                      // Nom de la base Railway
         $username = 'root';                       // User Railway
-        $password = 'PbEbisGsxdjaDmUZNSySHzyVNkYFqgof';   // Mot de passe Railway
+        $password = 'PbEbisGsxdjaDmUZNSySHzyVNkYFqgof';   
 
         $bdd = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $bdd->exec("SET NAMES utf8mb4");
+        $bdd->exec("SET time_zone = '+01:00'");
         return $bdd;
     }catch(PDOException $e){
         die($e->getMessage());
